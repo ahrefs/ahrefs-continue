f484b1122d07a2b597aaeb837954449c0aaefcd6
Calvinn Ng <calvinn.ng@ahrefs.com>
2024-05-02 18:32:35 +0800

    install all dependencies globally

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -12,14 +12,14 @@ popd
 
 echo "Installing GUI extension dependencies..."
 pushd gui
-npm install
+npm install -g
 npm link @continuedev/core
 popd
 
 echo "Installing VSCode extension dependencies..."
 pushd extensions/vscode
 # This does way too many things inline but is the common denominator between many of the scripts
-npm install
+npm install -g
 npm link @continuedev/core
 popd
 
@@ -30,10 +30,8 @@ popd
 
 echo "Installing binary dependencies..."
 pushd binary
-npm install
+npm install -g
 npm run build
-npm install -g @vscode/vsce
-npm install -g yarn
 
 
 # echo "Packaging extension..."
