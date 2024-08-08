5be9c217c054c75302e3a14638647d22dcd44704
Calvinn Ng <calvinn.ng@ahrefs.com>
2024-08-08 16:31:05 +0800

    remove debug print

--- core/config/load.ts
+++ core/config/load.ts
@@ -90,7 +90,6 @@ async function fetchRemoteConfig(url: string): Promise<any> {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
     const data = await response.json();
-    console.log(data);
     return data;
   } catch (error) {
     console.error('Error fetching data: ', error);
